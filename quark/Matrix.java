e7ff4503c31ac47d32a92011015625ea639ad4cb
Aman Sharma <lunchspider03@gmail.com>
2020-04-26 20:59:28 +0530

    git commit

--- quark/Matrix.java
+++ quark/Matrix.java
@@ -1,11 +1,12 @@
 package quark;
+
 public class Matrix {
-    private int matrix[][];
+    private double matrix[][];
     private int row,col;
     public Matrix(int rows,int columns){
         this.row = rows;
         this.col = columns;
-        this.matrix = new int[row][col];
+        this.matrix = new double[row][col];
     }
     public  Matrix(int order){
         // creates a square matrix of given order
@@ -17,7 +18,7 @@ public class Matrix {
     public int GetTotalColumn(){
         return this.col;
     }
-    public int[] GetRow(int n){
+    public double[] GetRow(int n){
         if(n>row || n<1){
             return null;
         }
@@ -25,18 +26,18 @@ public class Matrix {
             return this.matrix[n-1];
         }
     }
-    public int[] GetCol(int n){
+    public double[] GetCol(int n){
         if(n>col|| n<1){
             return null;
         }
         else{
-            int s[]= new int[this.col];
+            double s[]= new double[this.col];
             for(int i = 0;i<row;i++)
                 s[i]= this.matrix[i][n-1];
             return s;
         }
     }
-    public void Values(int rows,int columns,int value){
+    public void Values(int rows,int columns,double value){
         if(rows-1>this.row || columns-1>this.col|| rows<1||columns<1){
             return;
         }
@@ -44,7 +45,7 @@ public class Matrix {
             this.matrix[rows-1][columns-1] = value;
         }
     }
-    public int ValuesAt(int n,int j){
+    public double ValuesAt(int n,int j){
         if(n-1>this.row || j-1>this.col|| j<1||n<1){
             return 0;
         }
@@ -88,7 +89,27 @@ public class Matrix {
         }
         return x;
     }
+    public static Matrix Multiply(Matrix x,Matrix y){
+        Matrix Product = new Matrix(x.GetTotalRow(),y.GetTotalColumn());
+        for(int i = 1;i<=Product.GetTotalRow();i++){
+            for(int j = 1;j<=Product.GetTotalColumn();j++){
+                for(int k = 1;k<=x.GetTotalColumn();k++){
+                    Product.Values(i, j, Product.ValuesAt(i, j) + x.ValuesAt(i, k) * y.ValuesAt(k, j));
+                }
+            }
+        }
+        return Product;
+    }
     public static Matrix Multiply(Matrix...v){
-       return null;
+        Matrix Product= Matrix.Multiply(v[0],v[1]);
+        for(int l= 2;l<v.length;l++){
+            // multiplying product with v[l] from v[2] to v's lenght
+            Product = Matrix.Multiply(Product, v[l]);
+        }
+       return Product;
+    }
+    public double Det(Matrix y){
+
+        return 0;
     }
 }
